fce25bb7c19cdb8ecddb34d3cc5be202d7cd4328
Jorge Aparicio <jorge@japaric.io>
2018-02-26 19:20:34 +0100

    test building the book and do link checking first

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,11 @@
 set -euxo pipefail
 
 main() {
+    # test that building the book works
+    mdbook build
+
+    linkchecker book
+
     # first (fast) pass: check that examples compile
     for chapter in $(echo src/*); do
         if [ ! -f $chapter/Cargo.toml ]; then
@@ -18,11 +23,6 @@ main() {
         fi
         popd
     done
-
-    # test that building the book works
-    mdbook build
-
-    linkchecker book
 
     # second (slow) pass: check that examples link
     for chapter in $(echo src/*); do
