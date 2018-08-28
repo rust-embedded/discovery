166f0ff2d00b53219d0a3ef3b27fd9d3ee27189a
Jorge Aparicio <jorge@japaric.io>
2018-08-28 19:04:41 +0200

    use mdbook v0.2.x

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,17 @@
 set -euxo pipefail
 
 main() {
+    # install latest mdbook v0.2.x release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-lang-nursery/mdbook \
+                    | cut -d/ -f3 \
+                    | grep -E '^v0.2.[0-9]+$' \
+                    | sort --version-sort \
+                    | tail -n1)
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git rust-lang-nursery/mdBook \
-           --tag v0.1.5 \
+           --tag $tag \
            --target x86_64-unknown-linux-musl
 
     rustup target add thumbv7em-none-eabihf
