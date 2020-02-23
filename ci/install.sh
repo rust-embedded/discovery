4374767edc87f9203ed3a8d57fc05f2b6976bb0e
Tomasz Różański <rosto@tlen.pl>
2020-02-23 02:18:43 +0100

    Fix for f7e6553

--- ci/install.sh
+++ ci/install.sh
@@ -1,13 +1,13 @@
 set -euxo pipefail
 
 main() {
-    # install latest mdbook v0.2.x release
+    # install latest mdbook v0.3.x release
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-lang-nursery/mdbook \
                     | cut -d/ -f3 \
-                    | grep -E '^v0.2.[0-9]+$' \
+                    | grep -E '^v0.3.[0-9]+$' \
                     | sort --version-sort \
                     | tail -n1)
-    local tag="v0.2.1"
+    local tag="v0.3.5"
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
