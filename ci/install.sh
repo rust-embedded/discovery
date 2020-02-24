921014bf9f368bfc8fa7f963420c73906a548e67
Adam Greig <adam@adamgreig.com>
2020-02-24 00:47:15 +0000

    Update CI install to use latest mdbook, reverting 09753a1

--- ci/install.sh
+++ ci/install.sh
@@ -1,17 +1,17 @@
 set -euxo pipefail
 
 main() {
-    # install latest mdbook v0.2.x release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/rust-lang-nursery/mdbook \
+    local tag=$(git ls-remote --tags --refs --exit-code \
+                    https://github.com/rust-lang/mdbook \
                     | cut -d/ -f3 \
-                    | grep -E '^v0.2.[0-9]+$' \
+                    | grep -E '^v[0-9\.]+$' \
                     | sort --version-sort \
                     | tail -n1)
-    local tag="v0.2.1"
+
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
-           --git rust-lang-nursery/mdBook \
+           --git rust-lang/mdbook \
            --tag $tag \
            --target x86_64-unknown-linux-musl
 
