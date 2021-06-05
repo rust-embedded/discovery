7adf9b93f556df5e378695dffec6f9c274e46d62
Christian Meusel <christian.meusel@posteo.de>
2021-06-05 14:32:29 +0200

    Update PAC and GPIO usage for upcomming PAC/HAL

--- src/11-usart/auxiliary/src/lib.rs
+++ src/11-usart/auxiliary/src/lib.rs
@@ -7,18 +7,18 @@ extern crate panic_itm; // panic handler
 
 pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
-pub use stm32f3_discovery::stm32f3xx_hal::stm32::usart1;
+pub use stm32f3_discovery::stm32f3xx_hal::pac::usart1;
 
 use stm32f3_discovery::stm32f3xx_hal::{
     prelude::*,
     serial::Serial,
-    stm32::{self, USART1},
+    pac::{self, USART1},
     time::MonoTimer,
 };
 
 pub fn init() -> (&'static mut usart1::RegisterBlock, MonoTimer, ITM) {
     let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
@@ -30,8 +30,8 @@ pub fn init() -> (&'static mut usart1::RegisterBlock, MonoTimer, ITM) {
         () => {
             let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
 
-            let tx = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
-            let rx = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
+            let tx = gpioa.pa9.into_af7_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+            let rx = gpioa.pa10.into_af7_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
 
             (tx, rx)
         }
@@ -39,14 +39,14 @@ pub fn init() -> (&'static mut usart1::RegisterBlock, MonoTimer, ITM) {
         () => {
             let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
 
-            let tx = gpioc.pc4.into_af7(&mut gpioc.moder, &mut gpioc.afrl);
-            let rx = gpioc.pc5.into_af7(&mut gpioc.moder, &mut gpioc.afrl);
+            let tx = gpioc.pc4.into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
+            let rx = gpioc.pc5.into_af7_push_pull(&mut gpioc.moder, &mut gpioc.otyper, &mut gpioc.afrl);
 
             (tx, rx)
         }
     };
 
-    Serial::usart1(dp.USART1, (tx, rx), 115_200.bps(), clocks, &mut rcc.apb2);
+    Serial::usart1(dp.USART1, (tx, rx), 115_200.Bd(), clocks, &mut rcc.apb2);
     // If you are having trouble sending/receiving data to/from the
     // HC-05 bluetooth module, try this configuration instead:
     // Serial::usart1(dp.USART1, (tx, rx), 9600.bps(), clocks, &mut rcc.apb2);
