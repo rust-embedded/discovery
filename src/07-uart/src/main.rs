d9a7b8a13794e30e5d1c4a860a597880915d7452
Henrik Böving <hargonix@gmail.com>
2021-08-14 16:51:42 +0200

    Flush

--- src/07-uart/src/main.rs
+++ src/07-uart/src/main.rs
@@ -50,7 +50,8 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).ok();
+    nb::block!(serial.write(b'X')).unwrap();
+    nb::block!(serial.flush()).unwrap();
 
     loop {}
 }
