7ae76a0c1729471634c3634dd5dcf831ab79d7a1
Tony <tony3159292@gmail.com>
2024-10-27 18:48:32 -0400

    Re-added v1 feature flag in Cargo.toml and updated src/main.rs for compatibility

--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -1,18 +1,29 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write;
 use cortex_m_rt::entry;
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
 
+#[cfg(feature = "v1")]
 use microbit::{
+    hal::prelude::*,
+    hal::uart,
+    hal::uart::{Baudrate, Parity},
+};
+
+#[cfg(feature = "v2")]
+use microbit::{
+    hal::prelude::*,
     hal::uarte,
     hal::uarte::{Baudrate, Parity},
 };
 
-use embedded_hal_nb::serial::Write as _;
+#[cfg(feature = "v2")]
+use embedded_hal_nb::serial::Write;
+#[cfg(feature = "v2")]
 mod serial_setup;
+#[cfg(feature = "v2")]
 use serial_setup::UartePort;
 
 #[entry]
@@ -20,6 +31,7 @@ fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
+    #[cfg(feature = "v1")]
     let mut serial = {
         let serial = uarte::Uarte::new(
             board.UARTE0,
@@ -30,8 +42,18 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    write!(serial, "The quick brown fox jumps over the lazy dog.\r\n").unwrap();
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
+    nb::block!(serial.write(b'X')).unwrap();
     nb::block!(serial.flush()).unwrap();
 
     loop {}
